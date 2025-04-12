98d732dfd18042804096343d11baecfb3fbeeae0
Christopher McKay <mckay.christopher73@outlook.com>
2025-04-12 19:32:38 -0400

    update: VERSION

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@
  * This module simplifies form navigation, field management, workflow actions and transition definition, action interception and site information.,
  * automatically operating on the global cur_frm.
  *
- * @version 2.1.1
+ * @version 2.1.2
  * 
  * @module Utils
  */
