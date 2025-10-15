e3597fb7b22e8d9d35ce2d2c55f8a8a4c055f733
Christopher McKay <mckay.christopher73@outlook.com>
2025-10-15 11:09:26 -0400

    chore: bump version

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@
  * This module simplifies form navigation, field management, workflow actions and transition definition, action interception and site information.,
  * automatically operating on the global cur_frm.
  *
- * @version 2.5.0
+ * @version 2.6.2
  *
  * @module Utils
  */
